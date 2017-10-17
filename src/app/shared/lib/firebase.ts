d041614f2ddd135e5fada970bc5255f46c4f62c6
cyokodog <cyokodog@gmail.com>
2017-10-17 20:54:58 +0900

    firebaseの環境設定

--- src/app/shared/lib/firebase.ts
+++ src/app/shared/lib/firebase.ts
@@ -1,16 +1,21 @@
 import { Injectable } from '@angular/core';
 import { WindowRef } from './window-ref';
+import { firebaseConfig } from '../../../environments/firebase-config';
 
 @Injectable()
 export class Firebase {
 
+  firebase: any;
+
   constructor(
     private windowRef: WindowRef
   ) {
+    this.firebase = this.windowRef.nativeWindow['firebase'];
+    this.firebase.initializeApp(firebaseConfig);
   }
 
   get instance(): any {
-    return this.windowRef.nativeWindow['firebase'];
+    return this.firebase;
   }
 
 }
